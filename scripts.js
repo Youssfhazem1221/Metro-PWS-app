ba988b6953ddf211581fc4c19e676ea7c254c01a
Youssf Hazem <youssf.hazem1221@gmail.com>
2023-12-05 20:09:52 +0200

    updated CSS, Updated HTML elements, added two lines, created two arrays containing the values of the secondLine and the thirdLine

--- scripts.js
+++ scripts.js
@@ -2,13 +2,25 @@ function getDirection() {
   const currentStation = document.getElementById('currentStation').value;
   const destinationStation = document.getElementById('destinationStation').value;
 
- const metroStations = ["marj", "azbt", "ain shams", "matarya", "helmyazaton", "hadayekzaton", "saray", "hamamat", "kobry", "sadr", "demerdash", "ghamra", "shohadaa", "oraby", "naser", "sadat", "zaghloul", "zeinab", "saleh", "margergis", "zahraa", "darelsalam", "hadayekmaadi", "maadi", "thakanat", "torabalad", "kozzika", "turaelesmant", "masaraa", "hadayekhelwan", "wadihof", "helwanuni", "ainhelwan", "helwan"];
+ const firstLine = ["marj", "azbt", "ain shams", "matarya", "helmyazaton", "hadayekzaton", "saray", "hamamat", "kobry", "sadr", "demerdash", "ghamra", "shohadaa", "oraby", "naser", "sadat", "zaghloul", "zeinab", "saleh", "margergis", "zahraa", "darelsalam", "hadayekmaadi", "maadi", "thakanat", "torabalad", "kozzika", "turaelesmant", "masaraa", "hadayekhelwan", "wadihof", "helwanuni", "ainhelwan", "helwan"];
+
+ const secondLine = ["shubra", "mezalat", "khalafawi", "sainttrase", "rodelfarag", "masarrah", "shohadaa", "atabba", "abdeen", "sadat", "opera", "dokki", "bohooth", "cairouni", "faisal", "giza", "omelmasr", "sakyet", "moneb"];
+
+ const thirdLine = ["adly", "hayksteb", "omar", "qubaa", "heshambarakat", "nozha", "elshams", "alf", "heliopolis", "haroun", "alahram", "kollyet", "stadium", "fairzone", "abasya", "abdobasha", "geish", "babelsharia", "atabba", "naser", "masbero", "safaa", "kitkat"];
 
   const marjDirection = "Helwan";
   const helwanDirection = "Marj";
+  const adlyDirection = "kitkat";
+  const kitkatDirection = "Adly";
+  const shubraDirection = "Moneeb"; 
+  const monebDirection = "Shubra";
 
-  const currentIndex = metroStations.indexOf(currentStation);
-  const destinationIndex = metroStations.indexOf(destinationStation);
+  
+ 
+
+
+  const currentIndex = firstLine.indexOf(currentStation);
+  const destinationIndex = firstLine.indexOf(destinationStation);
 
   if (currentIndex < destinationIndex) {
       document.getElementById('result').textContent = "Take " + marjDirection + " direction.";
